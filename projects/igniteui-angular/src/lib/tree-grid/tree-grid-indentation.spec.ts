e34e84bd59393f5ae7a6819e92344ac12de5f021
Tacho <tjelev22@abv.bg>
2018-10-17 13:00:47 +0300

    test(tree-grid): add integration spec file #2530

--- projects/igniteui-angular/src/lib/tree-grid/tree-grid-indentation.spec.ts
+++ projects/igniteui-angular/src/lib/tree-grid/tree-grid-indentation.spec.ts
@@ -1,12 +1,10 @@
 import { async, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
 import { SortingDirection } from '../data-operations/sorting-expression.interface';
 import { IgxTreeGridComponent } from './tree-grid.component';
 import { IgxTreeGridModule } from './index';
 import { IgxTreeGridSimpleComponent, IgxTreeGridPrimaryForeignKeyComponent } from '../test-utils/tree-grid-components.spec';
 import { IgxNumberFilteringOperand } from '../data-operations/filtering-condition';
 import { TreeGridFunctions } from '../test-utils/tree-grid-functions.spec';
-import { UIInteractions, wait } from '../test-utils/ui-interactions.spec';
 
 describe('IgxTreeGrid - Indentation', () => {
     let fix;
@@ -23,7 +21,7 @@ describe('IgxTreeGrid - Indentation', () => {
             .compileComponents();
     }));
 
-    describe('Child Collection Indentation', () => {
+    describe('Child Collection', () => {
         beforeEach(() => {
             fix = TestBed.createComponent(IgxTreeGridSimpleComponent);
             fix.detectChanges();
@@ -125,70 +123,9 @@ describe('IgxTreeGrid - Indentation', () => {
             TreeGridFunctions.verifyRowIndentationLevel(treeGrid.getRowByIndex(0), rows[0], 0);
             TreeGridFunctions.verifyRowIndentationLevel(treeGrid.getRowByIndex(1), rows[1], 1);
         });
-
-        it('should transform a non-tree column into a tree column when pinning it', () => {
-            TreeGridFunctions.verifyTreeColumn(fix, 'ID', 4);
-
-            treeGrid.pinColumn('Age');
-            fix.detectChanges();
-
-            TreeGridFunctions.verifyTreeColumn(fix, 'Age', 4);
-        });
-
-        it('should transform a non-tree column into a tree column when hiding the original tree-column', () => {
-            TreeGridFunctions.verifyTreeColumn(fix, 'ID', 4);
-
-            const column = treeGrid.columns.filter(c => c.field === 'ID')[0];
-            column.hidden = true;
-            fix.detectChanges();
-
-            TreeGridFunctions.verifyTreeColumn(fix, 'Name', 3);
-        });
-
-        it('should transform the first visible column into tree column when pin and hide another column before that', () => {
-            TreeGridFunctions.verifyTreeColumn(fix, 'ID', 4);
-
-            treeGrid.pinColumn('Age');
-            fix.detectChanges();
-
-            const column = treeGrid.columns.filter(c => c.field === 'Age')[0];
-            column.hidden = true;
-            fix.detectChanges();
-
-            TreeGridFunctions.verifyTreeColumn(fix, 'ID', 3);
-        });
-
-        it('should transform a non-tree column into a tree column when moving the original tree-column through API', () => {
-            TreeGridFunctions.verifyTreeColumn(fix, 'ID', 4);
-
-            // Move tree-column
-            const sourceColumn = treeGrid.columns.filter(c => c.field === 'ID')[0];
-            const targetColumn = treeGrid.columns.filter(c => c.field === 'HireDate')[0];
-            treeGrid.moveColumn(sourceColumn, targetColumn);
-            fix.detectChanges();
-
-            TreeGridFunctions.verifyTreeColumn(fix, 'Name', 4);
-        });
-
-        it('should transform a non-tree column into a tree column when moving the original tree-column through UI', (async() => {
-            TreeGridFunctions.verifyTreeColumn(fix, 'ID', 4);
-
-            const column = treeGrid.columnList.filter(c => c.field === 'ID')[0];
-            column.movable = true;
-
-            const header = TreeGridFunctions.getHeaderCell(fix, 'ID').nativeElement;
-            UIInteractions.simulatePointerEvent('pointerdown', header, 50, 50);
-            UIInteractions.simulatePointerEvent('pointermove', header, 56, 56);
-            await wait();
-            UIInteractions.simulatePointerEvent('pointermove', header, 490, 30);
-            UIInteractions.simulatePointerEvent('pointerup', header, 490, 30);
-            fix.detectChanges();
-
-            TreeGridFunctions.verifyTreeColumn(fix, 'Name', 4);
-        }));
     });
 
-    describe('Primary/Foreign key Relation Indentation', () => {
+    describe('Primary/Foreign key', () => {
         beforeEach(() => {
             fix = TestBed.createComponent(IgxTreeGridPrimaryForeignKeyComponent);
             fix.detectChanges();
@@ -284,67 +221,6 @@ describe('IgxTreeGrid - Indentation', () => {
             TreeGridFunctions.verifyRowIndentationLevel(treeGrid.getRowByIndex(0), rows[0], 0);
             TreeGridFunctions.verifyRowIndentationLevel(treeGrid.getRowByIndex(1), rows[1], 1);
         });
-
-        it('should transform a non-tree column into a tree column when pinning it', () => {
-            TreeGridFunctions.verifyTreeColumn(fix, 'ID', 5);
-
-            treeGrid.pinColumn('Name');
-            fix.detectChanges();
-
-            TreeGridFunctions.verifyTreeColumn(fix, 'Name', 5);
-        });
-
-        it('should transform a non-tree column into a tree column when hiding the original tree-column', () => {
-            TreeGridFunctions.verifyTreeColumn(fix, 'ID', 5);
-
-            const column = treeGrid.columns.filter(c => c.field === 'ID')[0];
-            column.hidden = true;
-            fix.detectChanges();
-
-            TreeGridFunctions.verifyTreeColumn(fix, 'ParentID', 4);
-        });
-
-        it('should transform the first visible column into tree column when pin and hide another column before that', () => {
-            TreeGridFunctions.verifyTreeColumn(fix, 'ID', 5);
-
-            treeGrid.pinColumn('Age');
-            fix.detectChanges();
-
-            const column = treeGrid.columns.filter(c => c.field === 'Age')[0];
-            column.hidden = true;
-            fix.detectChanges();
-
-            TreeGridFunctions.verifyTreeColumn(fix, 'ID', 4);
-        });
-
-        it('should transform a non-tree column into a tree column when moving the original tree-column through API', () => {
-            TreeGridFunctions.verifyTreeColumn(fix, 'ID', 5);
-
-            // Move tree-column
-            const sourceColumn = treeGrid.columns.filter(c => c.field === 'ID')[0];
-            const targetColumn = treeGrid.columns.filter(c => c.field === 'JobTitle')[0];
-            treeGrid.moveColumn(sourceColumn, targetColumn);
-            fix.detectChanges();
-
-            TreeGridFunctions.verifyTreeColumn(fix, 'ParentID', 5);
-        });
-
-        it('should transform a non-tree column into a tree column when moving the original tree-column through UI', (async() => {
-            TreeGridFunctions.verifyTreeColumn(fix, 'ID', 5);
-
-            const column = treeGrid.columnList.filter(c => c.field === 'ID')[0];
-            column.movable = true;
-
-            const header = TreeGridFunctions.getHeaderCell(fix, 'ID').nativeElement;
-            UIInteractions.simulatePointerEvent('pointerdown', header, 50, 50);
-            UIInteractions.simulatePointerEvent('pointermove', header, 56, 56);
-            await wait();
-            UIInteractions.simulatePointerEvent('pointermove', header, 490, 30);
-            UIInteractions.simulatePointerEvent('pointerup', header, 490, 30);
-            fix.detectChanges();
-
-            TreeGridFunctions.verifyTreeColumn(fix, 'ParentID', 5);
-        }));
     });
 });
 
---
+++ projects/igniteui-angular/src/lib/tree-grid/tree-grid-integration.spec.ts
@@ -0,0 +1,160 @@
+import { async, TestBed } from '@angular/core/testing';
+import { IgxTreeGridComponent } from './tree-grid.component';
+import { IgxTreeGridModule } from './index';
+import { IgxTreeGridSimpleComponent, IgxTreeGridPrimaryForeignKeyComponent } from '../test-utils/tree-grid-components.spec';
+import { TreeGridFunctions } from '../test-utils/tree-grid-functions.spec';
+import { UIInteractions, wait } from '../test-utils/ui-interactions.spec';
+
+fdescribe('IgxTreeGrid - Integration', () => {
+    let fix;
+    let treeGrid: IgxTreeGridComponent;
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [
+                IgxTreeGridSimpleComponent,
+                IgxTreeGridPrimaryForeignKeyComponent
+            ],
+            imports: [IgxTreeGridModule]
+        })
+            .compileComponents();
+    }));
+
+    describe('Child Collection', () => {
+        beforeEach(() => {
+            fix = TestBed.createComponent(IgxTreeGridSimpleComponent);
+            fix.detectChanges();
+            treeGrid = fix.componentInstance.treeGrid;
+        });
+
+        it('should transform a non-tree column into a tree column when pinning it', () => {
+            TreeGridFunctions.verifyTreeColumn(fix, 'ID', 4);
+
+            treeGrid.pinColumn('Age');
+            fix.detectChanges();
+
+            TreeGridFunctions.verifyTreeColumn(fix, 'Age', 4);
+        });
+
+        it('should transform a non-tree column into a tree column when hiding the original tree-column', () => {
+            TreeGridFunctions.verifyTreeColumn(fix, 'ID', 4);
+
+            const column = treeGrid.columns.filter(c => c.field === 'ID')[0];
+            column.hidden = true;
+            fix.detectChanges();
+
+            TreeGridFunctions.verifyTreeColumn(fix, 'Name', 3);
+        });
+
+        it('should transform the first visible column into tree column when pin and hide another column before that', () => {
+            TreeGridFunctions.verifyTreeColumn(fix, 'ID', 4);
+
+            treeGrid.pinColumn('Age');
+            fix.detectChanges();
+
+            const column = treeGrid.columns.filter(c => c.field === 'Age')[0];
+            column.hidden = true;
+            fix.detectChanges();
+
+            TreeGridFunctions.verifyTreeColumn(fix, 'ID', 3);
+        });
+
+        it('should transform a non-tree column into a tree column when moving the original tree-column through API', () => {
+            TreeGridFunctions.verifyTreeColumn(fix, 'ID', 4);
+
+            // Move tree-column
+            const sourceColumn = treeGrid.columns.filter(c => c.field === 'ID')[0];
+            const targetColumn = treeGrid.columns.filter(c => c.field === 'HireDate')[0];
+            treeGrid.moveColumn(sourceColumn, targetColumn);
+            fix.detectChanges();
+
+            TreeGridFunctions.verifyTreeColumn(fix, 'Name', 4);
+        });
+
+        it('should transform a non-tree column into a tree column when moving the original tree-column through UI', (async () => {
+            TreeGridFunctions.verifyTreeColumn(fix, 'ID', 4);
+
+            const column = treeGrid.columnList.filter(c => c.field === 'ID')[0];
+            column.movable = true;
+
+            const header = TreeGridFunctions.getHeaderCell(fix, 'ID').nativeElement;
+            UIInteractions.simulatePointerEvent('pointerdown', header, 50, 50);
+            UIInteractions.simulatePointerEvent('pointermove', header, 56, 56);
+            await wait();
+            UIInteractions.simulatePointerEvent('pointermove', header, 490, 30);
+            UIInteractions.simulatePointerEvent('pointerup', header, 490, 30);
+            fix.detectChanges();
+
+            TreeGridFunctions.verifyTreeColumn(fix, 'Name', 4);
+        }));
+    });
+
+    describe('Primary/Foreign key', () => {
+        beforeEach(() => {
+            fix = TestBed.createComponent(IgxTreeGridPrimaryForeignKeyComponent);
+            fix.detectChanges();
+            treeGrid = fix.componentInstance.treeGrid;
+        });
+
+        it('should transform a non-tree column into a tree column when pinning it', () => {
+            TreeGridFunctions.verifyTreeColumn(fix, 'ID', 5);
+
+            treeGrid.pinColumn('Name');
+            fix.detectChanges();
+
+            TreeGridFunctions.verifyTreeColumn(fix, 'Name', 5);
+        });
+
+        it('should transform a non-tree column into a tree column when hiding the original tree-column', () => {
+            TreeGridFunctions.verifyTreeColumn(fix, 'ID', 5);
+
+            const column = treeGrid.columns.filter(c => c.field === 'ID')[0];
+            column.hidden = true;
+            fix.detectChanges();
+
+            TreeGridFunctions.verifyTreeColumn(fix, 'ParentID', 4);
+        });
+
+        it('should transform the first visible column into tree column when pin and hide another column before that', () => {
+            TreeGridFunctions.verifyTreeColumn(fix, 'ID', 5);
+
+            treeGrid.pinColumn('Age');
+            fix.detectChanges();
+
+            const column = treeGrid.columns.filter(c => c.field === 'Age')[0];
+            column.hidden = true;
+            fix.detectChanges();
+
+            TreeGridFunctions.verifyTreeColumn(fix, 'ID', 4);
+        });
+
+        it('should transform a non-tree column into a tree column when moving the original tree-column through API', () => {
+            TreeGridFunctions.verifyTreeColumn(fix, 'ID', 5);
+
+            // Move tree-column
+            const sourceColumn = treeGrid.columns.filter(c => c.field === 'ID')[0];
+            const targetColumn = treeGrid.columns.filter(c => c.field === 'JobTitle')[0];
+            treeGrid.moveColumn(sourceColumn, targetColumn);
+            fix.detectChanges();
+
+            TreeGridFunctions.verifyTreeColumn(fix, 'ParentID', 5);
+        });
+
+        it('should transform a non-tree column into a tree column when moving the original tree-column through UI', (async () => {
+            TreeGridFunctions.verifyTreeColumn(fix, 'ID', 5);
+
+            const column = treeGrid.columnList.filter(c => c.field === 'ID')[0];
+            column.movable = true;
+
+            const header = TreeGridFunctions.getHeaderCell(fix, 'ID').nativeElement;
+            UIInteractions.simulatePointerEvent('pointerdown', header, 50, 50);
+            UIInteractions.simulatePointerEvent('pointermove', header, 56, 56);
+            await wait();
+            UIInteractions.simulatePointerEvent('pointermove', header, 490, 30);
+            UIInteractions.simulatePointerEvent('pointerup', header, 490, 30);
+            fix.detectChanges();
+
+            TreeGridFunctions.verifyTreeColumn(fix, 'ParentID', 5);
+        }));
+    });
+});
